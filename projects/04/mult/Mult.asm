f976e78f0d106080ca87e8d94ebb098002a75c2a
JeongChanDo <djc4223@naver.com>
2022-05-17 08:02:36 +0000

    add first multi.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,44 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Put your code here
+// R0 = 3,  R1 = 4    1+1+1   1+1+1    1+1+1    1+1+1
+
+
+// if R0 or R1 == 0 -> end
+@R0
+D=M
+@BIG_STOP
+D;JEQ
+@R1
+@BIG_STOP
+D;JEQ
+@j
+D=1
+(BIG_LOOP)
+// if (j >R1) goto BIG_END
+@j
+D=M
+@R1
+D=D-M
+@BIG_STOP
+D;JGT
+// if r0 == 3,  sum==r2 += 3
+@i
+D=1
+(SMALL_LOOP)
+// if (i > R0) goto SMALL_END
+@i
+D=M
+@R0
+D=D-M
+@SMALL_STOP
+D;JGT // if i == (R0 + 1) goto SMALL_STOP
+@R2
+M=M+1
+@i
+M=M+1
+(SMALL_STOP)
+@j
+M=M+1
+(BIG_STOP)
